4aabb4c04f6a175ec9619374f19ea1244e026046
Naoki Ishida <naokiishida@shigeo-mbp-149.local>
2018-08-13 16:45:16 -0700

    Mempool class has a map object of tx.id and tx

---
+++ src/Mempool.h
@@ -0,0 +1,10 @@
+#include <map>
+#include "Transaction.h"
+`
+using namespace str;
+class Mempool{
+ public:
+  map<string, shared_ptr<Transaction> mempool;
+  void add_txn_to_mempool(shared_ptr<Transaction> transaction);
+};t 
+
