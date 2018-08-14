215bcd6a20256e8d69d98676f838c45ec6ae9c6e
Akioto <sharokku9@gmail.com>
2018-08-14 16:48:13 -0700

    created add_txn_to_mempool function in Mempool.h

--- src/Mempool.h
+++ src/Mempool.h
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <memory>
 #include <iostream>
-#include <
+#include <exception>
 
 #include "Transaction.h"
 
@@ -23,7 +23,7 @@ class Mempool{
     std::stringstream ss;
   }*/
 
-  void add_txn_to_mempool(shared_ptr<Transaction> transaction,
+  void addTxntoMempool(shared_ptr<Transaction> transaction,
                           shared_ptr<vector<string>> peer_hostnames);
   {
     if (mempool.find(transaction.id) == mempool.end()){
@@ -32,7 +32,7 @@ class Mempool{
       /*try {
         transaction = transaction.validateTransaction();
       }
-      catch(const transactionValidationError& e)
+      catch(transactionValidationError& e)
       {
        if(e.to_orphan == nullptr){
        cout << transaction rejected << endl;
@@ -51,15 +51,30 @@ class Mempool{
 
    }*/
 
-   
-
-
-
-
      }
     }
 
+    shared_ptr<UnspentTxOut> findUtxoinMempool(shared_ptr<TxIn>&& txin){
+      string txid = txin.toSpend.txid;
+      int txoutidx = txin.toSpend.txouIdx;
 
+      try{
+        txout = this->mempool[txid]->txouts[idx];
+      }
+      catch(const exception& e)
+      {
+      cout << "Could not find utxo in mempool for" << txid << endl;
+      return None;
+      }
+
+      //isCoinbase = false, height = -1
+      return UnspentTxOut(txout.value, txout.toAddr, txid,
+                          txoutidx, false, -1);
+
+      )
+
+
+    }
 
 
 
